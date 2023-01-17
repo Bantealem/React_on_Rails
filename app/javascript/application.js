3ce89f172b9b6791e5ad1f94d016bd39315e5322
bantealem <bantem2021@gmail.com>
2023-01-17 14:18:29 +0300

    Add Controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import React from 'react';
+import ReactDOM from 'react-dom';
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+function App() {
+  return (<h1>Hello World!</h1>);
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
